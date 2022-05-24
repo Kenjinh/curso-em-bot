f56e3e0f7b255689ef55d9853fe2ed4984a048a1
gnohara <gnohara@idc.com>
2022-05-24 12:46:33 -0300

    .

--- bot/wsgi.py
+++ bot/wsgi.py
@@ -1,7 +1,14 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bot.settings")
+"""
+WSGI config for library project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
 
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bot.settings')
+
+application = get_wsgi_application()
