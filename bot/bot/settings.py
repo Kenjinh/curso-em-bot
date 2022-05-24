37b993884d3c83d9167df98db04c751c75e16c81
gnohara <gnohara@idc.com>
2022-05-23 22:02:40 -0300

    Add libary and settings for run application on heroku

--- bot/bot/settings.py
+++ bot/bot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,3 +147,6 @@ CHATTERBOT = {
         'chatterbot.logic.BestMatch'
     ]
 }
+
+# Heroku settings
+django_heroku.settings(locals())
